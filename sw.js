e1de254ac65c9616ffa54332f757c38434a6b311
burim13 <purplessoccer@outlook.com>
2026-09-03 01:28:50 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline cache for the EA study app. Put this file next to ea-study.html.
-const CACHE="ea-part1-v3";
+const CACHE="ea-part1-v4";
 const FILES=["./","./index.html","./sw.js","./manifest.json","./icon-180.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
